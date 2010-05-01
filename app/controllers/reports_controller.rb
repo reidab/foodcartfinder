1d6fa44ec8e6c2f0a2ceb3c7e63a7867d74b4d20
Audrey Eschright <spinnerin@gmail.com>
2010-05-01 14:34:32 -0700

    Making form submit work and making things prettier.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,4 +4,15 @@ class ReportsController < ApplicationController
     @report = Report.new
   end
   
+  def create
+    @report = Report.new(params[:report])
+    if @report.save
+      flash[:success] = "Your report was successfully created!"
+      redirect_to :action => "new"
+    else
+      flash[:error] = "FAIL"
+      render :action => "new"
+    end
+  end
+  
 end
--- app/models/report.rb
+++ app/models/report.rb
@@ -1,4 +1,6 @@
 class Report < ActiveRecord::Base
   FOOD_TYPES = ['tasty', 'yucky', 'other']
+  PRICE_RANGES = [["< $5", 1], ["< $10", 2], ["< $15", 3], ["more", 4]]
+  
   serialize :food_types
 end
