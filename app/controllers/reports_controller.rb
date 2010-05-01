c48a752f26ae671cf1e2d84f83396be9a1b2621e
Audrey Eschright <spinnerin@gmail.com>
2010-05-01 15:12:23 -0700

    Adding Mapstraction with a simple map on the new report page.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,6 +2,10 @@ class ReportsController < ApplicationController
   
   def new
     @report = Report.new
+    @map = Mapstraction.new("map_div", :yahoo)
+    @map.control_init(:small => true)
+    @map.center_zoom_init([45.528479,-122.676086],16)
+    @map.marker_init(Marker.new([45.528479,-122.676086], :label => "Here", :info_bubble => "Info! Info!"))
   end
   
   def create
