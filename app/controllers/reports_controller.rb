94d3c0877a1b20e9ef3eb3f7a8414015b909c987
Audrey Eschright <spinnerin@gmail.com>
2010-05-01 17:17:14 -0700

    Updating default map points.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,7 +4,7 @@ class ReportsController < ApplicationController
     @report = Report.new
     @map = Mapstraction.new("map_div", :yahoo)
     @map.control_init(:small => true)
-    @map.center_zoom_init([45.528479,-122.676086],16)
+    @map.center_zoom_init([45.521094,-122.676274],17)
     # @map.marker_init(Marker.new([45.528479,-122.676086], :label => "Here", :info_bubble => "Info! Info!"))
   end
   
