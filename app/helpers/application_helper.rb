7a969ff27aa4be4c682b1c3bfbfd8e377d2635f7
Reid Beels <mail@reidbeels.com>
2010-05-01 17:38:09 -0700

    Don't load jqTouch, because it's getting in the way of submitting forms. (also, improved flash rendering by stealing code outright from another app)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,19 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def render_flash
+    unless flash.blank?
+      content_tag("div", :id => "flash", :class => "flash") do
+        flash.keys.sort{|a,b| a.to_s <=> b.to_s }.map do |key|
+          render_message(key, flash[key])
+        end.join("\n")
+      end
+    end
+  end
+
+  def render_message(type, content, label = true)
+    classes = ['message', type, (label ? 'with_label' : nil)]
+    content_tag("div", :class => classes.compact.join(' ')) do
+      (label ? content_tag(:span, type.to_s.titleize, :class => 'message_type') : '') << simple_format(content)
+    end
+  end
 end
