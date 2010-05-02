ef8c2faa212f42797ac850aa31f98d731cb5430c
Audrey Eschright <spinnerin@gmail.com>
2010-05-01 19:40:06 -0700

    Adding food type categories.

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,7 +1,8 @@
 class Report < ActiveRecord::Base
   FOOD_TYPES = ["American", "Asian", "BBQ", "Bento", "Bosnian", "Burritos", "Coffee", "Crepes", "Czech", "Dessert", "European", 
-    "Falafel", "Fried Foods", "Hot Dogs/Sausages", "Ice Cream", "Indian", "Italian", "Japanese", "Mediterranean", 
-    "Milkshakes/Smoothies", "Peruvian", "Pizza", "Polish", "Rice Bowl", "Sandwiches", "Soup", "Thai", "Vietnamese", "Waffles"]
+    "Falafel", "Fried Foods", "Hot Dogs/Sausages", "Ice Cream", "Indian", "Italian", "Japanese", "Mediterranean", "Mexican",
+    "Milkshakes/Smoothies", "Peruvian", "Pizza", "Polish", "Rice Bowl", "Sandwiches", "Seafood", "Soup", "Thai", "Vietnamese", 
+    "Waffles"]
   PRICE_RANGES = [["< $5", 1], ["< $10", 2], ["< $15", 3], ["more", 4]]
   DIETS = ['Vegan', 'Vegetarian', 'Gluten Free', 'Dairy Free', 'Kosher', 'Halal']
   
